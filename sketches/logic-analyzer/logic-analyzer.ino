15e5bc3d187ca70044076e25c8489191eb381369
P. Scott DeVos <scott.devos@sykes.com>
2023-11-01 13:55:32 -0400

    Improves Logic Analyzer output
    Converts data output to binary.

--- sketches/logic-analyzer/logic-analyzer.ino
+++ sketches/logic-analyzer/logic-analyzer.ino
@@ -18,6 +18,18 @@ void onClock() {
     unsigned int addr_high = PINC & 0b11111111;
     unsigned int data = PINL & 0b11111111;
     char output[30];
-    sprintf(output, "%02x %02x    %02x", addr_high, addr_low, data);
+
+    char bin[9] = "00000000";
+    byte2binary(data, bin);
+    sprintf(output, "%02x%02x    %s", addr_high, addr_low, bin);
     Serial.println(output);
+}
+
+void byte2binary(int b, char bin[]) {
+  for (int i = 0; i < 8; ++i) {
+    bin[7-i] = b & 1 ? '1' : '0';
+    b >>= 1;
+  }
+  bin[8] = '\0';
+  return bin;
 }
