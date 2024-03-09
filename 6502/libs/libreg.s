8df1d05341131b8e57e307dc1b172a9a4d1d85dc
P. Scott DeVos <scott.devos@draft2digital.com>
2024-03-09 07:33:26 -0500

    adjustments to LCD output to get more info

--- 6502/libs/libreg.s
+++ 6502/libs/libreg.s
@@ -1,20 +1,26 @@
 ; Depends on libhw.s
 
 ; Set video register bank and block
-;   Vars:   BNK_SEL     Boolean 0 = BG RAM, 1 = FG RAM
+;   Vars:   BNK_SEL     0 = None, 1 = BG RAM, 3-FF = FG RAM
 ;           BLK_SEL     Video RAM block 0 - 3
 set_video_reg:
     pha
     lda BNK_SEL         ; Load bank selection
-    and #%00000001      ; Only interested in low order bit
-    beq .bg
-    lda #%01000000      ; Set for FG RAM
+    bne .set_bnk        ; If bank is zero...
+    lda #%11000000      ; Set neither FG nor BG ram; use high speed clock
     jmp .set_blk
-.bg
-    lda #%10000000      ; Set for BG RAM
+.set_bnk
+    cmp #$1             ; check for BG bank
+    beq .set_bg
+    lda #%01100000      ; Set for FG RAM; use low speed clock
+    jmp .set_blk
+.set_bg
+    lda #%10100000      ; Set for BG RAM; use low speed clock
 .set_blk
     clc
     adc BLK_SEL         ; Set RAM block
     sta VID_RAM_REG
+    nop
+    nop
     pla
     rts
--- vga/sync_prom.py
+++ vga/sync_prom.py
@@ -3,22 +3,22 @@
 from io import IOBase
 
 #   7   6   5    4    3   2   1    0
-#   DI  BI  VCRB HCRB VBI HBI VSYB HSYB
+#   DI  BI  VCRB HCRB BIE BIS VSYB HSYB
 #   |   |   |    |    |   |   |    |
-#   |   |   |    |    |   |   |    |___Horizontal Sync
-#   |   |   |    |    |   |   |________Vertical Sync
-#   |   |   |    |    |   |____________Horizontal Blank Interval
-#   |   |   |    |    |________________Vertical Blank Interval
-#   |   |   |    |_____________________Horizontal Counter Reset
-#   |   |   |__________________________Vertical Counter Reset
+#   |   |   |    |    |   |   |    |___Horizontal Sync Bar
+#   |   |   |    |    |   |   |________Vertical Sync Bar
+#   |   |   |    |    |   |____________Blank Interval Starting (Now)
+#   |   |   |    |    |________________Blank Interval Ending (Soon)
+#   |   |   |    |_____________________Horizontal Counter Reset Bar
+#   |   |   |__________________________Vertical Counter Reset Bar
 #   |   |______________________________Blank Iterval
 #   |__________________________________Display Interval
 
 # Control Bits
 HSY = 0b00000001
 VSY = 0b00000010
-HBI = 0b00000100
-VBI = 0b00001000
+BIS = 0b00000100
+BIE = 0b00001000
 HCR = 0b00010000
 VCR = 0b00100000
 BI  = 0b01000000
@@ -42,28 +42,48 @@ VSY_START = 217     # Start of Vertical Sync
 VSY_END   = 221     # End of Vertical Sync
 VBI_END   = 244     # End of Vertical Blank Interval
 
+irq_recovery_time = 2   # The number of horz counter periods to "pad" the end
+                        # the end of of the blank interval for the BIE signal
 
-def set_H_bits(x: int, byte: int):
+def set_H_bits(x: int, y: int, signals: int):
+    """
+i   Sets the horizontal-related bits of the signals
+    x: horizontal counter value
+    y: vertical counter value
+    signals: signal bits
+    """
     # H Display Interval
     if x < HBI_START:
-        pass
+        # Pulse BIS at the start of the first line of the vertical blank
+        # interval
+        if y == VBI_START and x == 0:
+            signals |= BIS
     # H Front Porch
     elif x < HSY_START:
-        byte |= BI | HBI
+        # Pulse BIS at the start of the horizontal blank interval except
+        # during the vertical blank interval
+        if y < VBI_START and x == HBI_START:
+            signals |= BIS
+        signals |= BI
     # H Sync
     elif x < HSY_END:
-        byte |= BI | HBI
-        byte &= HSY_INV # hsync sig is inverted
+        signals |= BI
+        signals &= HSY_INV # hsync sig is inverted
     # H Back Porch
     elif x < HBI_END:
-        byte |= BI | HBI
+        # Pulse BIE near the end of the horizontal blank interval except
+        # during the vertical blanking interval lines prior to the last line
+        # I.e. do pulse on the last line of the VBI
+        if (y < VBI_START or y == VBI_END) and x == HBI_END - irq_recovery_time:
+            signals |= BIE
+        signals |= BI
     # H Counter Reset
     else:
         # Just need one horizontal counter reset normally, but in case system
-        # power up at an address "inbetween" scan lines, want to reset HCR to
+        # power up at an address "between" scan lines, want to reset HCR to
         # get things started
-        byte &= HCR_INV
-    return byte
+        signals &= HCR_INV
+    return signals
 
 
 def main(outfile: str):
@@ -76,36 +96,36 @@ def main(outfile: str):
             for x in range(256):
                 # sync and reset signals are inverted; set by default
                 # horizontal and vertical counter reset also set by default
-                byte = HSY | VSY | HCR | VCR
+                signals = HSY | VSY | HCR | VCR
 
                 # V Display Interval
                 if y < VBI_START:
                     # H Display Interval
                     if x < HBI_START:
-                        byte |= DI
-                    byte = set_H_bits(x, byte)
+                        signals |= DI
+                    signals = set_H_bits(x, signals)
                 # V Front Porch
                 elif y < VSY_START:
-                    byte |= BI | VBI
-                    byte = set_H_bits(x, byte)
+                    signals |= BI | BIE
+                    signals = set_H_bits(x, signals)
                 # V Sync
                 elif y < VSY_END:
-                    byte |= BI | VBI
-                    byte &= VSY_INV # vsync sig is inverted
-                    byte = set_H_bits(x, byte)
+                    signals |= BI | BIE
+                    signals &= VSY_INV # vsync sig is inverted
+                    signals = set_H_bits(x, signals)
                 # V Back Porch
                 elif y < VBI_END:
-                    byte |= BI | VBI
-                    byte = set_H_bits(x, byte)
+                    signals |= BI | BIE
+                    signals = set_H_bits(x, signals)
                 # V Counter Reset
                 else:
                     # Just need one vertical reset byte normally, but system
                     # could start at power up with any random address, so best
                     # to reset HCR and VCR until the end of the address space
-                    byte &= VCR_INV
-                    byte &= HCR_INV
+                    signals &= VCR_INV
+                    signals &= HCR_INV
 
-                fp.write(bytes([byte]))
+                fp.write(bytes([signals]))
 
 
 if __name__ == "__main__":
